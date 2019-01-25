1b612ec23ceb9edad4adfb22c3751f2bdffadac9
Martha Taylor <marthadigitalize@gmail.com>
2019-01-25 14:12:25 +0000

    accesibility + responsiveness + service worker add

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
@@ -178,7 +179,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  more.tabIndex = '3';
+  more.tabIndex = '4';
   li.append(more)
     
   return li
@@ -209,8 +210,6 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
-
 
 /**
  * Add Service Worker
--- sW.js
+++ sW.js
@@ -1,7 +1,11 @@
+/**
+  Code reuse: 
+  This was my first service worker and I took code from the two different youtube tutorials to create it
+  Some code Mohamed Riaad study jam at https://www.youtube.com/watch?v=TxXwlOAXUko and https://www.youtube.com/watch?v=BfL3pprhnms
+**/
 
-console.log('Service Worker: Registered');
 
-
+//main cache variable is given a value with a number of 1 as this will increment auto each time a new service worker called + cache stored a
  let mainCacheName = 'restauantCache-1';
 
  let urlCacheList = [
@@ -21,16 +25,18 @@ console.log('Service Worker: Registered');
  	'./img/7.jpg',
  	'./img/8.jpg',
  	'./img/9.jpg',
- 	'./img/10.jpg',
- 	'/js/sW.js'
+ 	'./img/10.jpg'
+ 	//'/sW.js'
  ];
 
 
-
- self.addEventListener('install', function(e){
+//installed event used with service worker to listen for and add urls to cache or if it can't to return an error if there is one
+ self.addEventListener('install', (e) => {
  	e.waitUntil(
+ 		//open cache folder then cache out caches
  		caches.open(mainCacheName).then((cache) => {
  			console.log(cache);
+ 			//add caches to the array of caches
  			return cache.addAll(urlCacheList);
  		}).catch(err => {
  			console.log(err);
@@ -38,44 +44,53 @@ console.log('Service Worker: Registered');
  		);
  });
 
+// activate event is waiting until the inner code is resolved before continuing so it will  delete old caches of restaur names and store new ones
+self.addEventListener('activate', function(e){
+	
+		e.waitUntil(
+			//searching through all the keys in the cache
+			caches.keys().then(function(cacheNames){
+				//do a promise all to loop through cache and resolve next few lines of code requests
+				return Promise.all( 
+					//filter through array of caches to create a new list of latest restaurant names
+					cacheNames.filter(function(cacheName){
+						return cacheName.startsWith('restaurant-') &&
+						cacheName !== mainCacheName;
+						//crete a new array of caches after old ones are deleted with the new service worker
+					}).map( function (cacheName) {
+						 return caches.delete(cacheName);
+					   })
+				);
+			})
+		);
+});
 
 
-
-// self.addEventListener('install', function (event) {
-
-//     event.waitUntil(
-//         caches.open(staticCacheName).then(function (cache) {
-//             console.log(cache);
-//             return cache.addAll(urlToCache);
-
-//         }).catch(erroe => {
-//             console.log(erroe);
-//         })
-//     );
+//would this es6 work ok?
+// self.addEventListener('activate', (e) => {
+// 		e.waitUntil(
+// 			caches.keys().then((cacheNames) => {
+// 				return Promise.all( 
+// 					cacheNames.filter((cacheName) => {
+// 						return cacheName.startsWith('restaurant') &&
+// 						cacheName !== mainCacheName;
+// 					}).map( (cacheName) => {
+// 						 return caches.delete(cacheName);
+// 					   })
+// 				);
+// 			})
+// 		);
 // });
 
-// self.addEventListener('activate', function (event) {
-//     event.waitUntil(
-//         caches.keys().then(function (cacheNames) {
-//             return Promise.all(
-//                 cacheNames.filter(function (cacheName) {
-//                     return cacheName.startsWith('restaurant-') &&
-//                         cacheName != staticCacheName;
-//                 }).map(function (cacheName) {
-//                     return caches.delete(cacheName);
-//                 })
-//             );
-//         })
-//     );
-// });
 
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
+//fetch request written in es6 hope it works ok, any advice on how to handle this better appreciated
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then( (response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
 
 
 
